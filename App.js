9590f49106e42f0f48a875e11b9d2ebeaa244fe7
Qsound224 <kesankee@gmail.com>
2025-06-17 22:39:59 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold text-red-600">Bienvenue sur Q-Sound & Event</h1>
+      <p className="mt-4 text-gray-700">Location de sonorisation, lumière, DJ, techniciens et plus encore.</p>
+    </div>
+  );
+}
+
+export default App;
